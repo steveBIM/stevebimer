ffe6b93dcc9fb4e72f04c69c7d06c634ff179e8d
imbytecat <imbytecat@gmail.com>
2023-01-31 13:56:13 +0800

    add: avif again

--- overrides/next.config.js
+++ overrides/next.config.js
@@ -13,10 +13,9 @@ module.exports = withBundleAnalyzer({
       'pbs.twimg.com',
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it',
       'cdn.lililili.net'
     ],
-    formats: ['image/webp'],
+    formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   }
